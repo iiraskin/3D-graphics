8f30c0585b41f196971bd63c0d8a88c82d37cbf6
Elena Gorskaya <helena557@mail.ru>
2020-04-17 01:22:16 +0300

    Update texture.cpp

--- Opengl_shooter/common/texture.cpp
+++ Opengl_shooter/common/texture.cpp
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "GL/glew.h"
+#include <GL/glew.h>
 
-#include "GLFW/glfw3.h"
+#include <GLFW/glfw3.h>
 
 
 GLuint loadBMP_custom(const char * imagepath){
